afecfd560c684e8c4cdf86263786954db2e26d44
Imadefc <imad.nova.mas@gmail.com>
2024-01-17 22:01:12 +0100

    Destructuring ejercicios

---
+++ Destructuring 2/exercise.js
@@ -0,0 +1,13 @@
+function isAdult(person) {
+  return person >= 18;
+}
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+let {id,firstName,lastName,age}=person;
+
+console.log(isAdult(age));
---
+++ Destructuring/exercise.js
@@ -0,0 +1,8 @@
+let arr = [10, 20];
+let [num1, num2]= arr;
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+
+// Variable swapping
+let [v, b]= arr;
+
+console.log('After swap: ', b, v); // After swap: 20 10
