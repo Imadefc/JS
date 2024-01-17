4012956291e4e1a1c896001130e9af58af03c21f
Imadefc <imad.nova.mas@gmail.com>
2024-01-18 00:13:02 +0100

    spread rest ejercicios

---
+++ Rest parameters and spread syntax 3/exercise.js
@@ -0,0 +1,11 @@
+function sum(arr) {
+  let sum =0;
+  for (let i = 0; i < arr.length; i++) {
+    sum+=arr[i];
+    
+  }
+  return sum;
+}
+
+const numbers = [1, 2, 3];
+console.log(sum(numbers));
