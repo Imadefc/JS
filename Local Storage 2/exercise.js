db0fd9f929dc0424d69ceed352a17d5575ab7d19
Imadefc <imad.nova.mas@gmail.com>
2024-02-02 20:25:58 +0100

    LocalStorage 1 y 2

---
+++ Local Storage 2/exercise.js
@@ -0,0 +1,8 @@
+const user1 = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+console.log(user1.name+": "+localStorage.getItem(user1.name));
+
---
+++ Local Storage/exercise.js
@@ -0,0 +1,8 @@
+
+const user = {
+  id: 1,
+  name: "John",
+  age: 25,
+};
+
+localStorage.setItem(user.name, user.age);
