71b96939a4ffabaa235ab8d928533b25e6d340fa
Imadefc <imad.nova.mas@gmail.com>
2024-02-05 14:10:45 +0100

    Arreglo Todo-List

--- Network Requests - Todo List/exercise.js
+++ Network Requests - Todo List/exercise.js
@@ -1,13 +1,10 @@
 let url="https://jsonplaceholder.typicode.com/todos";
 
-fetch(url)
-.then((str)=>str.json())
-.then((st)=>{
-    for (const el of st) {
-        let msg="";
-        msg += el.title+" : ";
-        msg+=el.completed==true?"✔":"❌";
-        console.log(msg);
-    }
-})
-.catch((err)=>console.log("Algo a salido mal: "+err))
+async function llamada(url){
+    const response = await fetch(url);
+    const data =await response.json();
+    console.log(data);
+    return data;
+}
+
+llamada(url);
